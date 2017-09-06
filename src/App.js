f5221153e3e3894540b26a9baf053f064e05cbdd
Lewis Daly <lewis.daly@enabled.com.au>
2017-09-06 15:58:11 +0930

    embed 4pbot survey

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
 
-import Header from './Header'
 import Banner from './Banner'
 import Content from './Content'
 import Footer from './Footer'
@@ -11,8 +10,7 @@ class App extends Component {
 
   render() {
     return (
-      <div className="sans-serif">
-        <Header/>
+      <div>
         <Banner/>
         <Content/>
         <Footer/>
--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
-/**
- * React Static Boilerplate
- * Copyright (c) 2015-present Kriasoft. All rights reserved.
- */
-
-/* @flow */
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+
 import 'tachyons'
 
 import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
+import Survey from './Survey/Survey'
+import Header from './Header'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+ReactDOM.render(
+  <Router>
+    <div className="sans-serif">
+      <Header/>
+      <Route exact path='/' component={App} />
+      <Route path='/survey' component={Survey}/>
+    </div>
+  </Router>,
+  document.getElementById('root')
+);
